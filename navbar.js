5ac72cb566afc8eba2287642c23e8cb8c3d25514
leadaviaud <lea.daviaud@lycee-elie-vinet.org>
2025-05-08 15:18:50 +0200

    nouveaux liens

--- navbar.js
+++ navbar.js
@@ -1,8 +1,12 @@
-// navbar.js
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
-            document.querySelector('body').insertAdjacentHTML('afterbegin', data);
+            document.body.insertAdjacentHTML('afterbegin', data);
+            const toggleBtn = document.querySelector('.navbar-toggle');
+            const links = document.querySelector('.navbar-links');
+            toggleBtn.addEventListener('click', () => {
+                links.classList.toggle('show');
+            });
         });
 });
